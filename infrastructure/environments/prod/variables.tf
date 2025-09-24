c5d68b6f56ed49a4a1e364c4e26561e607088eff
Franklyn101 <franklyninoma@gmail.com>
2025-09-24 10:03:42 +0100

    fix

--- infrastructure/environments/prod/variables.tf
+++ infrastructure/environments/prod/variables.tf
@@ -2,14 +2,17 @@
 variable "region" {
   description = "AWS region"
   type        = string
+    default     = "us-north-1"
 }
 
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
+   default     = "innovatemart-eks-cluster"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR block"
   type        = string
+  default     = "10.0.0.0/16"
 }
